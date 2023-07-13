b2165e12c214e2035632bc7d53b1ae871a39f4c3
Carolien C.H.M. Maas <96486516+CHMMaas@users.noreply.github.com>
2023-07-13 15:59:00 +0200

    Delete Development/Code/Old code directory

--- Development/Code/Old code/anova.glm.lrm.R
+++
@@ -1,4 +0,0 @@
-# LRT test with glm
-drop1(glm(form.full.model, data=mice::complete(imputed.data, m), family="binomial"), test="Chisq")
-# Wald-test = coef / SE^2
-an.full <- anova(lrm(form.full.model, data=mice::complete(imputed.data, m)))
